a28e1bb48a75cb2a2f7006dd101314f1441c3df3
Chan Saechao <chan.saechao3@pcc.edu>
2020-05-19 03:56:52 -0700

    print date

--- src/cis133j/lab4/helloworld/HelloWorld.java
+++ src/cis133j/lab4/helloworld/HelloWorld.java
@@ -3,5 +3,6 @@ package cis133j.lab4.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.prinln("Hello, World!");
+        System.out.println(new Date());
     }
 }
